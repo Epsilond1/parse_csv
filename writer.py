ec1344d2f55ef09cad3a1cf2f3076151fb718ebe
Semyon Poltorak <epsilond1@yandex.ru>
2017-12-08 22:03:31 +0500

    color fix

--- writer.py
+++ writer.py
@@ -9,28 +9,26 @@ class Writer(object):
     def record(self):
         font0 = xlwt.Font()
         font0.name = 'Times New Roman'
-        font0.colour_index = 2
+        font0.colour_index = 8
         font0.bold = True
 
         style0 = xlwt.XFStyle()
         style0.font = font0
 
-        style1 = xlwt.XFStyle()
-        style1.num_format_str = 'D-MMM-YY'
-
         wb = xlwt.Workbook()
         # Шапка документа
         sheet = wb.add_sheet('Результаты импорта')
         sheet.write(0, 0, 'Элементная база', style0)
-        sheet.write(0, 1, 'Хар-ка элементов', style0)
-        sheet.write(0, 2, 'Количество элементов', style0)
+        sheet.write(0, 1, 'Описание', style0)
+        sheet.write(0, 2, 'Хар-ка элементов', style0)
+        sheet.write(0, 3, 'Количество элементов', style0)
 
         # Заполнение документа
         row_num = 1
         for element in self.info_element:
             sheet.write(row_num, 0, '{0} - {1}'.format(element[1], element[2]), style0)
-            sheet.write(row_num, 1, '{0}'.format(element[0].character), style0)
-            sheet.write(row_num, 2, '{0}'.format(element[0].count), style0)
+            sheet.write(row_num, 2, '{0}'.format(element[0].character), style0)
+            sheet.write(row_num, 3, '{0}'.format(element[0].count), style0)
             row_num += 1
 
         wb.save('example.xls')
